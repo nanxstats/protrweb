ed1293f64d36c62fc12eaabd04bec4bd926d66ec
Nan Xiao <road2stat@gmail.com>
2017-06-08 05:37:02 -0400

    house keeping

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
 library('shiny')
 library('protr')
 
-extractCTD = function (x) return(c(extractCTDC(x), extractCTDT(x), extractCTDD(x)))
+extractCTD = function (x) c(extractCTDC(x), extractCTDT(x), extractCTDD(x))
 
 shinyServer(function(input, output, session) {
 
@@ -21,15 +21,22 @@ shinyServer(function(input, output, session) {
 
   observe({
     # switch tab
-    if ( (!is.null(input$file1) | !is.null(input$file2)) & !is.null(input$variable) & input$protrsubmitButton != 0L ) {
-      updateTabsetPanel(session, 'protrwebmain', selected = 'Computed Descriptors')
+    if ( (!is.null(input$file1) |
+          !is.null(input$file2)) &
+         !is.null(input$variable) &
+         input$protrsubmitButton != 0L ) {
+      updateTabsetPanel(
+        session, 'protrwebmain', selected = 'Computed Descriptors')
     }
   })
 
   observe({
     # switch tab
-    if ( ((input$text1 != 'Paste FASTA file content here') | (input$text2 != 'Paste raw sequence here')) & !is.null(input$variable) & input$protrsubmitButton != 0L ) {
-      updateTabsetPanel(session, 'protrwebmain', selected = 'Computed Descriptors')
+    if ( ((input$text1 != 'Paste FASTA file content here') |
+          (input$text2 != 'Paste raw sequence here')) &
+         !is.null(input$variable) & input$protrsubmitButton != 0L ) {
+      updateTabsetPanel(
+        session, 'protrwebmain', selected = 'Computed Descriptors')
     }
   })
 
@@ -39,24 +46,34 @@ shinyServer(function(input, output, session) {
     fmtType = input$formatType
     iptType = input$inputType
 
-    if ( fmtType == 'fasta' & iptType == 'uploadfile' & !is.null(input$file1) ) {
+    if ( fmtType == 'fasta' &
+         iptType == 'uploadfile' &
+         !is.null(input$file1) ) {
       seq = readFASTA(input$file1$datapath)
     }
 
-    if ( fmtType == 'fasta' & iptType == 'pastecontent' & (input$text1 != 'Paste FASTA file content here') ) {
+    if ( fmtType == 'fasta' &
+         iptType == 'pastecontent' &
+         (input$text1 != 'Paste FASTA file content here') ) {
       seq = readFASTA(textConnection(input$text1))
     }
 
-    if ( fmtType == 'rawseq' & iptType == 'uploadfile' & !is.null(input$file2) ) {
+    if ( fmtType == 'rawseq' &
+         iptType == 'uploadfile' &
+         !is.null(input$file2) ) {
       seq = scan(input$file2$datapath, what = 'complex', blank.lines.skip = TRUE)
     }
 
-    if ( fmtType == 'rawseq' & iptType == 'pastecontent' & (input$text2 != 'Paste raw sequence here') ) {
+    if ( fmtType == 'rawseq' &
+         iptType == 'pastecontent' &
+         (input$text2 != 'Paste raw sequence here') ) {
       seq = scan(textConnection(input$text2), what = 'complex', blank.lines.skip = TRUE)
     }
 
     if ( is.null(input$variable) ) {
+
       return(NULL)
+
     } else if ( length(as.character(input$variable)) >= 1L ) {
 
       exec = paste0('t(sapply(seq, ', funcdict[as.character(input$variable)], '))')
@@ -66,7 +83,7 @@ shinyServer(function(input, output, session) {
 
         n = length(exec)
 
-        for ( i in 1L:n ) {
+        for (i in 1L:n) {
           outlist[[i]] = eval(parse(text = exec[i]))
           incProgress(1/n, detail = paste('Finished Part', i))
         }
@@ -88,9 +105,11 @@ shinyServer(function(input, output, session) {
   })
 
   output$downloadData = downloadHandler(
-    filename = function() { paste('ProtrWeb-', paste(input$variable, collapse = '-'),
-                                  '-', gsub(' ', '-', gsub(':', '-', Sys.time())),
-                                  '.csv', sep = '') },
+    filename = function() {
+      paste('ProtrWeb-', paste(input$variable, collapse = '-'),
+            '-', gsub(' ', '-', gsub(':', '-', Sys.time())),
+            '.csv', sep = '')
+    },
     content = function(file) {
       csvTable = descTable()
       write.csv(csvTable, file)
--- ui.R
+++ ui.R
@@ -1,104 +1,117 @@
 library('shiny')
 library('protr')
 
-# dark actionButton
+# primary action button
 nx.actionButton = function (inputId, label, icon = NULL) {
   if (!is.null(icon))
-    buttonContent <- list(icon, label)
-  else buttonContent <- label
-  tags$button(id = inputId, type = 'button', class = 'btn btn-primary action-button',
-              buttonContent)
+    buttonContent = list(icon, label) else
+      buttonContent = label
+    tags$button(
+      id = inputId, type = 'button',
+      class = 'btn btn-primary action-button',
+      buttonContent)
 }
 
-shinyUI(fluidPage(theme = 'cerulean.css',
+shinyUI(fluidPage(
 
-                  fluidRow(column(12, includeHTML('header.html'))),
+  theme = 'cerulean.css',
 
-                  headerPanel(title = 'ProtrWeb', windowTitle = 'ProtrWeb'),
+  fluidRow(column(12, includeHTML('header.html'))),
 
-                  sidebarPanel(
+  headerPanel(title = 'ProtrWeb', windowTitle = 'ProtrWeb'),
 
-                    radioButtons('formatType', 'Step 1. Choose file format',
-                                 c('FASTA' = 'fasta',
-                                   'Raw sequence' = 'rawseq')),
+  sidebarPanel(
 
-                    radioButtons('inputType', 'Step 2. Upload or paste?',
-                                 c('Upload file' = 'uploadfile',
-                                   'Paste file content' = 'pastecontent')),
+    radioButtons(
+      'formatType', 'Step 1. Choose file format',
+      c('FASTA' = 'fasta', 'Raw sequence' = 'rawseq')),
 
-                    strong('Step 3. Upload or paste protein sequence'),
+    radioButtons(
+      'inputType', 'Step 2. Upload or paste?',
+      c('Upload file' = 'uploadfile', 'Paste file content' = 'pastecontent')),
 
-                    conditionalPanel(
-                      a('Click here to download an example FASTA file', href='example.fasta'),
-                      br(),
-                      condition = "input.formatType == 'fasta' & input.inputType == 'uploadfile'",
-                      fileInput('file1', 'Upload FASTA file')
-                    ),
+    strong('Step 3. Upload or paste protein sequence'),
 
-                    conditionalPanel(
-                      a('Click here to download an example FASTA file', href='example.fasta'),
-                      br(),
-                      condition = "input.formatType == 'fasta' & input.inputType == 'pastecontent'",
-                      tags$textarea(id = 'text1', rows = 5, cols = 60, 'Paste FASTA file content here')
-                    ),
+    conditionalPanel(
+      a('Click here to download an example FASTA file', href='example.fasta'),
+      br(),
+      condition = "input.formatType == 'fasta' & input.inputType == 'uploadfile'",
+      fileInput('file1', 'Upload FASTA file')
+    ),
 
-                    conditionalPanel(
-                      a('Click here to download an example raw sequence file', href='example.txt'),
-                      br(),
-                      condition = "input.formatType == 'rawseq' & input.inputType == 'uploadfile'",
-                      fileInput('file2', 'Upload raw sequence file')
-                    ),
+    conditionalPanel(
+      a('Click here to download an example FASTA file', href='example.fasta'),
+      br(),
+      condition = "input.formatType == 'fasta' & input.inputType == 'pastecontent'",
+      tags$textarea(id = 'text1', rows = 5, cols = 60, 'Paste FASTA file content here')
+    ),
 
-                    conditionalPanel(
-                      a('Click here to download an example raw sequence file', href='example.txt'),
-                      br(),
-                      condition = "input.formatType == 'rawseq' & input.inputType == 'pastecontent'",
-                      tags$textarea(id = 'text2', rows = 5, cols = 60, 'Paste raw sequence here')
-                    ),
+    conditionalPanel(
+      a('Click here to download an example raw sequence file', href='example.txt'),
+      br(),
+      condition = "input.formatType == 'rawseq' & input.inputType == 'uploadfile'",
+      fileInput('file2', 'Upload raw sequence file')
+    ),
 
-                    tags$hr(),
-                    strong('Step 4. Select descriptor type(s)'),
-                    checkboxGroupInput('variable', 'Descriptor type (dimension):',
-                                       c('Amino Acid Composition (20)' = 'aac',
-                                         'Dipeptide Composition (400)' = 'dc',
-                                         'Tripeptide Composition (8000)' = 'tc',
-                                         'Normalized Moreau-Broto Autocorrelation (240)' = 'mb',
-                                         'Moran Autocorrelation (240)' = 'moran',
-                                         'Geary Autocorrelation (240)' = 'geary',
-                                         'C/T/D (21 + 21 + 105)' = 'ctd',
-                                         'Conjoint Triad (343)' = 'ctriad',
-                                         'Sequence-Order-Coupling Number (60)' = 'socn',
-                                         'Quasi-Sequence-Order Descriptors (100)' = 'qso',
-                                         'Pseudo-Amino Acid Composition (50)' = 'paac',
-                                         'Amphiphilic Pseudo-Amino Acid Composition (80)' = 'apaac')
-                    ),
+    conditionalPanel(
+      a('Click here to download an example raw sequence file', href='example.txt'),
+      br(),
+      condition = "input.formatType == 'rawseq' & input.inputType == 'pastecontent'",
+      tags$textarea(id = 'text2', rows = 5, cols = 60, 'Paste raw sequence here')
+    ),
 
-                    hr(),
+    tags$hr(),
+    strong('Step 4. Select descriptor type(s)'),
+    checkboxGroupInput(
+      'variable', 'Descriptor type (dimension):',
+      c('Amino Acid Composition (20)' = 'aac',
+        'Dipeptide Composition (400)' = 'dc',
+        'Tripeptide Composition (8000)' = 'tc',
+        'Normalized Moreau-Broto Autocorrelation (240)' = 'mb',
+        'Moran Autocorrelation (240)' = 'moran',
+        'Geary Autocorrelation (240)' = 'geary',
+        'C/T/D (21 + 21 + 105)' = 'ctd',
+        'Conjoint Triad (343)' = 'ctriad',
+        'Sequence-Order-Coupling Number (60)' = 'socn',
+        'Quasi-Sequence-Order Descriptors (100)' = 'qso',
+        'Pseudo-Amino Acid Composition (50)' = 'paac',
+        'Amphiphilic Pseudo-Amino Acid Composition (80)' = 'apaac')
+    ),
 
-                    nx.actionButton('protrsubmitButton', 'Compute selected descriptors', icon('check'))
+    hr(),
 
-                  ),
+    nx.actionButton(
+      'protrsubmitButton',
+      'Compute selected descriptors', icon('check'))
 
-                  mainPanel(
+  ),
 
-                    tabsetPanel(id = 'protrwebmain',
-                                tabPanel('Introduction',
-                                         includeHTML('introtext.html')
-                                ),
+  mainPanel(
 
-                                tabPanel('Computed Descriptors',
-                                         h3('Computed Descriptors'),
-                                         tags$hr(),
-                                         tableOutput('desc'),
-                                         tags$hr(),
-                                         downloadButton('downloadData', 'Download as CSV',
-                                                        class = 'btn btn-primary btn-large')
-                                )
+    tabsetPanel(
 
-                    ),
+      id = 'protrwebmain',
 
-                    includeHTML('footer.html')
+      tabPanel(
+        'Introduction',
+        includeHTML('introtext.html')
+      ),
 
-                  )
+      tabPanel(
+        'Computed Descriptors',
+        h3('Computed Descriptors'),
+        tags$hr(),
+        tableOutput('desc'),
+        tags$hr(),
+        downloadButton(
+          'downloadData', 'Download as CSV',
+          class = 'btn btn-primary btn-large')
+      )
+
+    ),
+
+    includeHTML('footer.html')
+
+  )
 
 ))
